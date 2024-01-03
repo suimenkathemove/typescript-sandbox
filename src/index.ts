2ba362a99d7448651ef72a198ff4f0b1c63a0b13
suimenkathemove <yoshiharu2580@gmail.com>
2024-01-04 01:32:13 +0900

    add ci

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { add } from "@/add";
 
 const main = () => {
+  // eslint-disable-next-line no-console
   console.log("Hello, World!");
 
+  // eslint-disable-next-line no-console
   console.log(add(1, 2));
 };
 
